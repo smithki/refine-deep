418949b5dff41b558e47fef8e273f0e763fd6f27
Ian K Smith <smithki707@gmail.com>
2018-12-22 12:18:58 -0600

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface RefineOptions {
+  ignoreNil?: boolean;
+  ignoreNull?: boolean;
+  ignoreUndefined?: boolean;
+  ignoreEmptyAny?: boolean;
+  ignoreEmptyArrays?: boolean;
+  ignoreEmptyObjects?: boolean;
+  ignoreEmptyStrings?: boolean;
+}
